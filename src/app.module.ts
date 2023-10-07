7c833d039286984a8268a8f978a273121835d315
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air.local>
2023-10-08 02:45:13 +0530

    Connection to database established and created table

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    PostModule,
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      database:'blog-tutorial',
+      username:'root',
+      password:'Nikhil@2209',
+      port:3306,
+      autoLoadEntities:true,
+      synchronize:true
+
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
